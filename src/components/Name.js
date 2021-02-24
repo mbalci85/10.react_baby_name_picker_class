3020fbe44aa07bbac25e43c4ee28ab35c929b17a
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-02-24 23:54:56 +0000

    added names as buttons

--- src/components/Name.js
+++ src/components/Name.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export class Name extends Component {
 	render() {
 		return (
-			<div>
-				<button>{this.props.name.name}</button>
+			<div style={{ display: 'inline-block' }}>
+				<button style={{ border: 'none' }}>{this.props.name.name}</button>
 			</div>
 		);
 	}
