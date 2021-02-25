3a7ae7dd312c40fba8a059a475e68fbb84865229
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-02-25 20:13:50 +0000

    added style to names div

--- src/components/Names.js
+++ src/components/Names.js
@@ -4,7 +4,16 @@ import Name from './Name';
 export class Names extends Component {
 	render() {
 		return (
-			<div>
+			<div
+				style={{
+					padding: 20,
+					borderRight: 'solid 5px lightpink',
+					borderBottom: 'solid 5px lightpink',
+					borderTop: 'solid 5px lightblue',
+					borderLeft: 'solid 5px lightblue',
+					borderRadius: 10,
+				}}
+			>
 				{console.log(this.props.names)}
 				{this.props.names.map((name) => (
 					<Name key={name.id} name={name} />
