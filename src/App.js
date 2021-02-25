3e2cb6b2f1eaebadd60b9a12dd5a1625888093b3
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-02-25 00:03:49 +0000

    added input box and buttons to filter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import NameFilter from './components/NameFilter';
 import Names from './components/Names';
 import names from './Names';
 
 export class App extends Component {
 	render() {
 		return (
-			<div style={{ marginLeft: 30, marginRight: 30 }}>
+			<div style={{ marginLeft: 30, marginRight: 30, textAlign: 'center' }}>
 				<h1 style={{ textAlign: 'center', marginTop: 15, fontSize: 65 }}>
 					BABY NAME PICKER
 				</h1>
+				<NameFilter />
 				<Names names={names} />
 			</div>
 		);
--- src/components/NameFilter.js
+++ src/components/NameFilter.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 export class NameFilter extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<div>
+				<input />
+				<button>All</button>
+				<button>Boys</button>
+				<button>Girls</button>
+			</div>
+		);
 	}
 }
 
